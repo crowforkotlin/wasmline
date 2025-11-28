c6772727ed191098748ccdc6064d786285568562
WuYa <crowforkotlin@gmail.com>
2025-11-29 03:19:01 +0800

    :test: test...

--- wasmtime-kotlin/wasmtime-app/android/src/androidMain/kotlin/crow/wasmtime/wasmline/MainActivity.kt
+++ wasmtime-kotlin/wasmtime-app/android/src/androidMain/kotlin/crow/wasmtime/wasmline/MainActivity.kt
@@ -20,7 +20,6 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 import kotlin.system.measureTimeMillis
-import kotlin.time.measureTime
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             .info()
         binding.load.setOnClickListener {
             binding.content.text = "Loading..."
-            testThread()
+            runWasm()
         }
     }
 
@@ -48,7 +47,6 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch(Dispatchers.IO) {
             try {
 
-                repeat(100) {
                     // 准备文件：将 assets 里的 plugin.wasm 拷贝到 cache 目录
                     // 因为我们的 C++ 层现在只接受文件路径，防止 OOM
                     val wasmFile = File(cacheDir, "plugin.wasm")
@@ -77,9 +75,6 @@ class MainActivity : AppCompatActivity() {
                     withContext(Dispatchers.Main) {
                         binding.content.text = "Result: $result\nTime: ${System.currentTimeMillis() - start}ms"
                     }
-
-                    delay(1)
-                }
 
                                 // 4. (可选) 释放模块
 //                 module.release()
