04ba8073cb4dbccfec6e189fe7fac7a60b5b854d
crowforkotlin <crowforkotlin@gmail.com>
2026-01-08 15:02:57 +0800

    ci: update GitHub Actions

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -10,7 +10,12 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+          search={{
+            options: {
+              type: 'static', // 关键：告诉前端使用静态搜索模式
+            },
+          }}>{children}</RootProvider>
       </body>
     </html>
   );
